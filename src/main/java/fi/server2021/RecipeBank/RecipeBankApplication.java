f4cdf256a589c604e25e31fa18e4159250e040db
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-10-05 14:47:38 +0300

    Create project

---
+++ src/main/java/fi/server2021/RecipeBank/RecipeBankApplication.java
@@ -0,0 +1,13 @@
+package fi.server2021.RecipeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/server2021/RecipeBank/RecipeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.server2021.RecipeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
